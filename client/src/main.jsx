b2934b7c533304fd2a19aa7be1485530a3400cdb
Oliver Winkler <winkloid@webauthn-localdev.fritz.box>
2023-04-13 13:54:59 +0200

    :construction: Began to implement some react-router routes in frontend.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
 import App from './App'
 import './index.css'
 
+import Root from "./routes/root.jsx";
+import ErrorPage from "./errorpage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
