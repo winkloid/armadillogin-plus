85395c365ee4f731b70c926bf72f446500aef905
Oliver Winkler <winkler.oliver@posteo.net>
2023-04-19 13:31:33 +0200

    :goal_net: Better error handling for registrationOptions endpoint: Now we distinguish between a complete new user, an already registered user and an user that never completed the registration process before.

--- server/controllers/webauthn.controller.js
+++ server/controllers/webauthn.controller.js
@@ -18,16 +18,36 @@ const registrationOptions = async (req, res) => {
     if(!req.body.userName) {
         return res.status(400).send("Kein Benutzername angegeben. Bitte geben Sie einen Benutzernamen an.");
     }
-    const existsUser = await UserModel.exists({
+    const isUserRegistered = await UserModel.findOne({
         userName: req.body.userName
-    }).then((databaseResponse) => {
-        return {success: 1, content: databaseResponse};
+    }).exec().then(async (databaseResponse) => {
+        if(!databaseResponse) {
+            return {success: 1, content: false};
+        } else {
+            // if a user with the specified username was found and is already registered, return true
+            if(databaseResponse.isRegistered) {
+                return {success: 1, content: true};
+
+            // if a user with the specified username was found but its registration is not yet complete
+            // this is necessary here because the database shall not be spammed with usernames that can't be used anymore after this
+            } else {
+                // if a username already exists in the database but the registration process for it was not completed yet, the user is deleted in order to create a new one later
+                // this is done to handle race conditions so that two users can never have the same user id to register an authenticator
+                return await UserModel.deleteOne({userName: req.body.userName, isRegistered: false}).exec().then((deletionResponse) => {
+                    return {success: 1, content: false};
+                }).catch((error) => {
+                    return {success: 0, content: error};
+                });
+            }
+        }
     }).catch((error) => {
         return {success: 0, content: error};
     });
 
-    if(existsUser.success) {
-        if(existsUser.content) {
+    // if request for user was successful
+    if(isUserRegistered.success) {
+        //if there is (content !== null) in the response, i.e. if an user was found
+        if(isUserRegistered.content) {
             return res.status(400).send("Der Benutzername existiert bereits und kann nicht erneut vergeben werden. Bitte wählen Sie einen anderen Benutzernamen.");
         } else {
             // if the user does not exist yet, we can create a new one
@@ -56,7 +76,7 @@ const registrationOptions = async (req, res) => {
             return res.status(200).send(options);
         }
     } else {
-        return res.status(500).send("Internal Server Error: \n\n" + existsUser.content);
+        return res.status(500).send("Internal Server Error: \n\n" + isUserRegistered.content);
     }
 }
 
@@ -70,9 +90,6 @@ async function createNewUser(userName) {
         return {success: 0, content: error};
     })
 }
-
-
-
 
 // only for debugging
 const writeUserToDb = (req, res) => {
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,8 +4,9 @@ const { v4: uuidv4 } = require("uuid");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    _id: { type: String, default: function generateUUID() { return uuidv4(); }},
-    userName: {type: String, required: true},
+    _id: {type: String, default: function generateUUID() { return uuidv4(); }},
+    userName: {type: String, required: true, unique: true},
+    isRegistered: {type: Boolean, default: false, required:true},
     currentUserChallenge: {type: String},
 });
 
